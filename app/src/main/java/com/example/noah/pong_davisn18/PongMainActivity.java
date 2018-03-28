d033e222897a6c996c3dfba18fb8a9486fcf2e3c
davisn18 <davisn18@up.edu>
2018-03-27 17:31:44 -0700

    Pong Part B complete!

--- app/src/main/java/com/example/noah/pong_davisn18/PongMainActivity.java
+++ app/src/main/java/com/example/noah/pong_davisn18/PongMainActivity.java
@@ -1,11 +1,15 @@
 package com.example.noah.pong_davisn18;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * PongMainActivity
@@ -13,17 +17,27 @@ import android.widget.Spinner;
  * This is the activity for the Pong game. It attaches a PongAnimator to
  * an AnimationSurface.
  *
- * Just run the app and watch the ball bounce off the paddle and walls.
- * Press restart for a new ball once the other goes out of play
+ * Just run the app and try to move the paddle so that it stays within play
+ * Press New Ball button for a new ball once the ball goes out of play
  * Change the difficulty to have different paddle sizes
+ * Once out of lives, user will be prompted with message, can close application
+ * or can start over with 3 lives and a score of 0 again.
  *
- * Enhancements:
+ * Enhancements Part A:
  *  1) When a ball leaves the field of play, new ball isn't added until the user
  *     presses the restart button
  *  2) Allow the user to change the size of the paddle through the difficult setting
  *     by selecting the level through the spinner
  *  3) Compatible on any device (Pixel C or Nexus 9, etc.) and can be played in both
  *     horizontal and landscape orientation
+ *
+ * Enhancements Part B:
+ *  1) When a ball leaves the field of play, the player loses a life. The player is allotted
+ *     3 lives and once there is none remaining, the game is over and the user is prompted
+ *     with a message to either start over or quit.
+ *  2) Keep a running score, plus 1 for each paddle hit and -3 for a missed hit
+ *  3) Incorporated randomness to the ball's bouncing velocity when hitting walls/paddle
+ *     (sometimes will speed up, sometimes will slow down, sometimes will have no change)
  *
  * @author Andrew Nuxoll
  * @author Steven R. Vegdahl
@@ -33,8 +47,13 @@ import android.widget.Spinner;
  */
 public class PongMainActivity extends Activity {
 
-    public boolean restart = false;
+    private Context context = this;
+    public boolean gameOver; //out of lives
+    public boolean newBall = false; //when newBall is in play
+    public boolean restartClickedBN = false; //used when ball is out of play with button presses
+    public boolean holdBall = false; //hold ball out of animation surface
     public int difficulty;
+    public int numLives = 3, score = 0; //keep track of number of lives and score of user
     private int check = 0; //used to not call code when first time spinner is pressed
 
     /**
@@ -50,13 +69,65 @@ public class PongMainActivity extends Activity {
                 .findViewById(R.id.animationSurface);
         mySurface.setAnimator(new TestAnimator(this, mySurface));
 
-        Button bnRestart = (Button)this.findViewById(R.id.bnRestart);
-        bnRestart.setOnClickListener(new View.OnClickListener() {
+        final TextView livesNum = (TextView) findViewById(R.id.numLivesTV);
+
+        Button newBallBN = (Button)this.findViewById(R.id.bnRestart);
+        newBallBN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                restart = true;
+                //used so that user can't press new ball a bunch and keep having lives decrement
+                if (!restartClickedBN) {
+                    restartClickedBN = true;
+                    numLives--; //decrement lives
+                    newBall = true; //newBall is in play
+                    holdBall = false; //no longer holding ball
+                    if (!gameOver)
+                        livesNum.setText(String.valueOf(numLives)); //set text to remaining lives
+
+                /*
+                 * code below sets up alert dialog pop up when user tries to restart without any lives left
+                 *
+                 * External Citation
+                 * Date: 26 March 2018
+                 * Problem: Didn't know how to create a pop up for the user when game is over
+                 * Resource: https://www.mkyong.com/android/android-alert-dialog-example/
+                 * Solution: I used the example code from this post.
+                 */
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                    // set title
+                    alertDialogBuilder.setTitle("Game Over");
+                    // set dialog message
+                    alertDialogBuilder
+                            .setMessage("You are out of lives! Your score was " + score + ". You suck!\n" +
+                                    "Would you like to play again or be a quitter?")
+                            .setCancelable(false)
+                            .setPositiveButton("Start Over", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // if this button is clicked, reset game (score & number of lives)
+                                    score = 3; //starts score at -3 if set to 0 so score is 0 when set to 3
+                                    numLives = 3;
+                                    livesNum.setText(String.valueOf(numLives)); //set text to remaining lives
+                                    gameOver = false;
+                                }
+                            })
+                            .setNegativeButton("Exit Application", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // if this button is clicked, just close
+                                    // the dialog box and do nothing
+                                    System.exit(0);
+                                }
+                            });
+                    // create alert dialog
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    //show dialog when user is out of lives so game is over
+                    if (numLives == -1 && gameOver)
+                        alertDialog.show();
+
+                }
             }
         });
+
+
 
         Spinner levels = (Spinner)findViewById(R.id.spLevel);
         levels.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -68,9 +139,8 @@ public class PongMainActivity extends Activity {
                  External Citation
                  Date: 18 March 2018
                  Problem: Select Item code would run when first clicking on the spinner
-                 Resource:
-                 http:https://stackoverflow.com/questions/13397933/
-                 android-spinner-avoid-onitemselected-calls-during-initialization
+                 Resource: http:https://stackoverflow.com/questions/13397933/
+                    android-spinner-avoid-onitemselected-calls-during-initialization
                  Solution: I used the example code from this post.
                  */
                 if (++check > 1) {
@@ -83,8 +153,5 @@ public class PongMainActivity extends Activity {
                 //do nothing
             }
         });
-
-
-
     }
 }
--- app/src/main/java/com/example/noah/pong_davisn18/TestAnimator.java
+++ app/src/main/java/com/example/noah/pong_davisn18/TestAnimator.java
@@ -1,10 +1,15 @@
 package com.example.noah.pong_davisn18;
 
+import android.app.AlertDialog;
 import android.app.Application;
+import android.content.DialogInterface;
 import android.graphics.*;
+import android.text.InputType;
 import android.view.MotionEvent;
+import android.widget.EditText;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * class that animates a ball repeatedly moving diagonally on
@@ -21,15 +26,15 @@ public class TestAnimator implements Animator {
     private Random random = new Random(); //random function
     private boolean start = true; //used to determine if start of app
 
+    private int padCenter; //location of center of paddle
+
     //ball instance variables
-    private int xVelocity;
-    private int yVelocity;
+    private double xVelocity, yVelocity;
     private int xVelDir; //positive(1) or negative(-1) x direction
-    private int yVelDir; ////positive(1) or negative(-1) y direction
-    private float ballXPos;
-    private float ballYPos;
+    private int yVelDir; //positive(1) or negative(-1) y direction
+    private double randSpeed;
+    private float ballXPos, ballYPos;
     private int radius = 30; //radius of ball
-    private boolean holdBall = false; //hold ball out of animation surface
 
     public TestAnimator(PongMainActivity activity, AnimationSurface surface) {
         this.main = activity;
@@ -67,22 +72,24 @@ public class TestAnimator implements Animator {
 
         int width = animSurface.getWidth();
         int height = animSurface.getHeight();
-        //used to determine if it's the start of the app, sets ball to center
+        int wallWidth = 50;
+        int padHeight = 50;
+        int padHalf;
+
+        //used to determine if it's the start of the app, sets ball and paddle to center
         if (start) {
             ballXPos = animSurface.getWidth() / 2;
             ballYPos = animSurface.getHeight() / 2;
+            padCenter = width / 2;
         }
 
-        int wallWidth = 50;
-        int padHeight = 50;
-        int halfPaddle;
-
+        //set paddle variable to correct size depending on difficulty
         if (main.difficulty == 0)
-            halfPaddle = 200;
+            padHalf = 200;
         else if (main.difficulty == 1)
-            halfPaddle = 125;
+            padHalf = 125;
         else
-            halfPaddle = 50;
+            padHalf = 50;
 
         Paint wallPaint = new Paint();
         wallPaint.setColor(0xFF39FF14); //neon green
@@ -90,17 +97,32 @@ public class TestAnimator implements Animator {
         paddlePaint.setColor(Color.WHITE);
         Paint ballPaint = new Paint();
         ballPaint.setColor(Color.WHITE);
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.RED);
+        textPaint.setTextSize(120f);
         //draw walls
         g.drawRect(0, 0, wallWidth, height, wallPaint); //left wall
         g.drawRect(0, 0, width, wallWidth, wallPaint); //top wall
         g.drawRect(width - wallWidth, 0, width, height, wallPaint); //right wall
+        //draw text for score
+        g.drawText("Score: " + main.score, width / 2.4f, height / 2, textPaint);
+
+        //check if paddle is hitting wall; stop paddle if it hits wall
+        if (padCenter - padHalf - wallWidth <= 0) //left wall
+            padCenter = wallWidth + padHalf;
+        else if (padCenter + padHalf + wallWidth >= width) //right wall
+            padCenter = width - wallWidth - padHalf;
         //draw paddle
-        g.drawRect(width / 2 - halfPaddle, height - padHeight, width / 2 + halfPaddle, height, paddlePaint);
-        //draw ball in correct position
-        g.drawCircle(ballXPos, ballYPos, radius, ballPaint);
+        g.drawRect(padCenter - padHalf, height - padHeight, padCenter + padHalf, height, paddlePaint);
+
+        if (!main.gameOver || !main.holdBall) //don't draw ball if game is over or new ball button isn't pressed yet
+            //draw ball in correct position
+            g.drawCircle(ballXPos, ballYPos, radius, ballPaint);
+
+        randomVelocity();
 
         //check for collisions
-        ballChecker(width, height, wallWidth, padHeight, halfPaddle);
+        ballChecker(width, height, wallWidth, padHeight, padHalf);
 
         updateBall(); //update ball's position
         start = false; //no longer is the start of the app
@@ -109,6 +131,7 @@ public class TestAnimator implements Animator {
     /*
      * collision checker, checks if hits walls or paddle and
      * responds appropriately to the collision
+     * multiplies velocities by random speed to change velocity a bit
      *
      * @param width - width of surface view
      * @param height - height of surface view
@@ -116,54 +139,68 @@ public class TestAnimator implements Animator {
      * @param padHeight - height of paddle
      * @param halfPaddle - half the width of the paddle
      */
-    private void ballChecker(int width, int height, int wallWidth, int padHeight , int halfPaddle) {
+    private void ballChecker(int width, int height, int wallWidth, int padHeight, int halfPaddle) {
         //top left corner, if ball hits directly in the corner, both x & y velocities are flipped
         if (ballXPos <= (wallWidth + radius) && ballYPos <= (wallWidth + radius)) {
-            xVelocity = -xVelocity;
-            yVelocity = -yVelocity;
+            xVelocity = -xVelocity * randSpeed;
+            yVelocity = -yVelocity * randSpeed;
         }
         //top right corner, if ball hits directly in the corner, both x & y velocities are flipped
         else if (ballXPos >= ((width - wallWidth) - radius) && ballYPos <= (wallWidth + radius)) {
-            xVelocity = -xVelocity;
-            yVelocity = -yVelocity;
+            xVelocity = -xVelocity * randSpeed;
+            yVelocity = -yVelocity * randSpeed;
         }
         //left wall
         else if (ballXPos <= (wallWidth + radius))
-            xVelocity = -xVelocity;
-            //top wall
+            xVelocity = -xVelocity * randSpeed;
+        //top wall
         else if (ballYPos <= (wallWidth + radius))
-            yVelocity = -yVelocity;
-            //right wall
+            yVelocity = -yVelocity * randSpeed;
+        //right wall
         else if (ballXPos >= (width - wallWidth) - radius)
-            xVelocity = -xVelocity;
-            //bottom and hit paddle at the left corner, switch both velocities
-        else if (ballXPos >= (width / 2 - halfPaddle - radius) && ballXPos <= (width / 2 - halfPaddle)
+            xVelocity = -xVelocity * randSpeed;
+        //bottom and hit paddle at the left corner, switch both velocities
+        else if (ballXPos >= (padCenter - halfPaddle - radius) && ballXPos <= (padCenter - halfPaddle)
                 && ballYPos >= (height - padHeight - radius)) {
-            xVelocity = -xVelocity;
-            yVelocity = -yVelocity;
+            xVelocity = -xVelocity * randSpeed;
+            yVelocity = -yVelocity * randSpeed;
+            main.score++;
         }
         //bottom and hit paddle at the right corner, switch both velocities
-        else if (ballXPos <= (width / 2 + halfPaddle + radius) && ballXPos >= (width / 2 + halfPaddle)
+        else if (ballXPos <= (padCenter + halfPaddle + radius) && ballXPos >= (padCenter + halfPaddle)
                 && ballYPos >= (height - padHeight - radius)) {
-            xVelocity = -xVelocity;
-            yVelocity = -yVelocity;
+            xVelocity = -xVelocity * randSpeed;
+            yVelocity = -yVelocity * randSpeed;
+            main.score++;
         }
         //bottom and hit paddle
-        else if (ballYPos >= (height - padHeight - radius) && ballXPos >= (width / 2 - halfPaddle)
-                && ballXPos <= (width / 2 + halfPaddle))
-            yVelocity = -yVelocity;
-            //bottom and missed paddle
+        else if (ballYPos >= (height - padHeight - radius) && ballXPos >= (padCenter - halfPaddle)
+                && ballXPos <= (padCenter + halfPaddle)) {
+            yVelocity = -yVelocity * randSpeed;
+            main.score++;
+        }
+        //bottom and missed paddle
         else if (ballYPos >= (height + radius)) {
-            if (main.restart) {
-                newVelocity();
-                ballXPos = animSurface.getWidth() / 2;
-                ballYPos = animSurface.getHeight() / 2;
-                main.restart = false; //reset boolean for restart button
-                holdBall = false; //release ball
-                updateBall(); //update it's position
+            if (!main.gameOver) { //if game is over, don't start another ball
+                //restartClicked
+                if (main.newBall && !main.holdBall) {
+                    newVelocity();
+                    main.holdBall = false; //release ball
+                    ballXPos = animSurface.getWidth() / 2;
+                    ballYPos = animSurface.getHeight() / 2;
+                    if (!main.gameOver) //don't lose points when game is over
+                        main.score -= 3; //lose 3 points when miss ball
+                    main.newBall = false; //reset boolean for restart (new ball) button
+
+                    updateBall(); //update it's position
+                } else {
+                    main.holdBall = true; //restart isn't pressed so don't release a new ball
+                    main.restartClickedBN = false;
+                    if (main.holdBall && main.numLives == 0) { //check if game is over
+                        main.gameOver = true;
+                    }
+                }
             }
-            else
-                holdBall = true; //restart isn't pressed so don't release a new ball
         }
     }
 
@@ -187,10 +224,21 @@ public class TestAnimator implements Animator {
     }
 
     /*
+     * Helper method to give the ball a new random velocity when hitting walls/paddle
+     */
+    private void randomVelocity() {
+        double max = 1.2;
+        double min = 0.8;
+        //random double between 0.8 and 1.2
+        randSpeed = min + (max - min) * random.nextDouble();
+    }
+
+
+    /*
      * Helper method to update position of ball
      */
     private void updateBall() {
-        if (!holdBall) {
+        if (!main.holdBall) {
             //set ball position to velocity every tick so ball constantly updates
             ballXPos += xVelocity;
             ballYPos += yVelocity;
@@ -216,11 +264,19 @@ public class TestAnimator implements Animator {
     }
 
     /**
-     *
+     * External Citation
+     * Date: 25 March 2018
+     * Problem: Didn't know how to use MotionEvent to get paddle position
+     * Resource: https://stackoverflow.com/questions/29792496/
+     * motionevent-action-down-in-android-is-too-sensitive-this-event-is-received-even
+     * Solution: I used the example code from this post.
      */
     public void onTouch(MotionEvent event) {
+        int action = event.getAction();
+        int x = (int) event.getX();
+        if (action == MotionEvent.ACTION_MOVE)
+            padCenter = x;
     }
-
 
 }//class TextAnimator
 
